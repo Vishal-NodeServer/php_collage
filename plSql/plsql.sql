54ef9b927b6ec5b7c4ba5d3ff13e9081b3ee203c
vishal suthar <vishal123suthar2@gmail.com>
2024-03-07 13:56:11 +0530

    cursor

--- plSql/plsql.sql
+++ plSql/plsql.sql
@@ -7,26 +7,26 @@
    var1 := 'vishal';
    num1 := 020;
    dbms_output.put_line(var1);
-   dbms_output.put_line('number is '||num1 );
+   dbms_output.put_line('number is '|| num1 );
    end;
  
-  /
+  
 
   declare
-  name varchar2(10);
+  namee varchar2(10);
   sal number(10,2);
 
   begin
-  select first_name , salary into name,sal from employees
-  where employees_id = 100;
-  dbms_output.put_line('Name : ' || name);
+  select  name , salary into namee,  sal from employees
+  where id = 200;
+  dbms_output.put_line('Name : ' || namee);
   dbms_output.put_line('salary : '|| sal);
   end;
   /
 
   
   declare
-  name employees.first_name%TYPE; //type attribute to take a same data type from table
+  name employees.name%TYPE; --type attribute to take a same data type from table
   sal employees.salary%TYPE;
   lastname name%TYPE;
   begin
@@ -38,14 +38,64 @@
   end;
   /
 
-   ////Select the all row data form the table
+--Select the all row data form the table
 
 declare 
 record employees%ROWTYPE;
 begin
 select * into record from employees
-where employees_id = 100;
-dbms_output.put_line(record.first_name  || record.lastname);
+where id = 100;
+dbms_output.put_line('Your name is : '|| record.name);
+dbms_output.NEW_line;
+dbms_output.put_line('Your Income : '|| record.salary );
 end;
 /
 
+
+--While loop
+declare
+a number(2) := 10;
+BEGIN 
+ WHILE a < 20 LOOP
+   dbms_output.put_line('value of a : ' || a);
+
+   a := a + 1;
+   IF a = 15 THEN
+     
+     a := a + 1;
+     CONTINUE;
+     END IF;
+     END LOOP;
+     END;
+
+--While with EXIT 
+declare
+a number(2) := 10;
+BEGIN 
+ WHILE a < 20 LOOP
+   dbms_output.put_line('value of a : ' || a);
+
+   a := a + 1;
+   IF a = 15 THEN
+     
+  
+     EXIT;
+     END IF;
+     END LOOP;
+     END;
+
+
+--for loop to print table of 5
+
+declare 
+n number := 10;
+t number;
+result number;
+BEGIN
+t:= &n;
+for i IN 1..n loop
+result := t * i;
+dbms_output.put_line(result);
+END LOOP;
+end;
+/
